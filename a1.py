f16c162aa3a4e3ff980136f8a12b2bdaec6e4828
Brown <brown@mysuperdispatch.com>
2020-03-19 21:02:12 +1100

    Use f-string format

--- a1.py
+++ a1.py
@@ -3,4 +3,4 @@ from mpi4py import MPI
 import sys
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
-print("Helloworld! I am process %d of %d.\n" % (rank, size))
+print(f"Hello world! I am a process {rank} of {size}.")
