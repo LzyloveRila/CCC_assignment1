0a685a07dd3493a2b49f82e07eba494653981971
karimoff <hey@karimoff.me>
2020-04-12 20:30:16 +1000

    Refactor

--- parallel_main.py
+++ parallel_main.py
@@ -1,13 +1,20 @@
-# Import MPI so we can run on more than one node and processor
+# Import functools to use writing decoretor functions
 import functools
-import json
-import sys
-import time
-from json.decoder import JSONDecodeError
 
+# Import json to be able to read JSON formatted data
+import json
+
+# Import sys to read CLI arguments
+import sys
+
+# Import time to measure runtime of the program
+import time
+
+# Import MPI so we can run on more than one node and processor
 from mpi4py import MPI
 
-# Constants
+
+# CONSTANTS
 
 DEFAULT_FILE = "smallTwitter.json"
 COUNTRY_CODE_FILE = "country_code.json"
@@ -32,44 +39,52 @@ def get_input_file(argv):
     return DEFAULT_FILE
 
 
-def tweet_processor(rank, processes, file):
-    lang_freq = {}
-    hashtag_frequency = {}
+def timer(func):
+    """Measure and print the runtime of the decorated function"""
 
-    with open(file, "r") as f:
-        for i, line in enumerate(f):
-            if i % processes == rank:
-                try:
-                    line = line[:-2]
-                    line = json.loads(line)
-                except JSONDecodeError:
-                    print("A line cannot be read ocurred, skip it")
-                    continue
-                except Exception as e:
-                    print(e)
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        started_at = time.perf_counter()
+        value = func(*args, **kwargs)
+        ended_at = time.perf_counter()
+        run_time = ended_at - started_at
+        print(f"Finished {func.__name__!r} in {run_time:.4f} seconds")
+        return value
 
-                # get a line now (data)
-                try:
-                    text = line["doc"]["text"]
-                    language = line["doc"]["metadata"]["iso_language_code"]
-                    if language != "":
-                        if language in lang_freq.keys():
-                            lang_freq[language] += 1
-                        else:
-                            lang_freq[language] = 1
+    return wrapper_timer
 
-                    hashtags = line["doc"]["entities"]["hashtags"]
-                    for hashtag in hashtags:
-                        hash_t = hashtag["text"].lower()
 
-                        if hash_t in hashtag_frequency:
-                            hashtag_frequency[hash_t] += 1
-                        else:
-                            hashtag_frequency[hash_t] = 1
-                except:
-                    print("could not interpret json")
+def print_top_languages(languages):
+    print(HORIZONTAL_LINE)
+    print("-------Top 10 Languages-----------")
+    print(HORIZONTAL_LINE)
+    for index, language in enumerate(languages, start=1):
+        print(f"{index} : {language}")
 
-    return lang_freq, hashtag_frequency
+
+def print_top_hashtags(hashtags):
+    print(HORIZONTAL_LINE)
+    print("--------Top 10 Hashtags-----------")
+    print(HORIZONTAL_LINE)
+    for index, hashtag in enumerate(hashtags, start=1):
+        print(f"{index} : {hashtag[0]} {hashtag[1]}")
+
+
+def match_country(top_10):
+    with open(COUNTRY_CODE_FILE, "r") as f:
+        country_code = json.load(f)
+
+    # search country code to get country name
+    top_10_list = []
+    for item in top_10:
+        if item[0] in country_code.keys():
+            top_10_list.append(
+                country_code[item[0]] + "(" + item[0] + "), " + str(item[1])
+            )
+        else:
+            top_10_list.append(item[0] + ", " + str(item[1]))
+
+    return top_10_list
 
 
 def marshall_freq(comm, lang_freq, htag_freq):
@@ -92,55 +107,70 @@ def marshall_freq(comm, lang_freq, htag_freq):
     return top_10_lang, top_10_htag
 
 
-def match_country(top_10):
-    with open(COUNTRY_CODE_FILE, "r") as f:
-        country_code = json.load(f)
+def tweet_processor(rank, processes, file):
+    language_occurences = {}
+    hashtag_occurences = {}
 
-    # search country code to get country name
-    top_10_list = []
-    for item in top_10:
-        if item[0] in country_code.keys():
-            top_10_list.append(
-                country_code[item[0]] + "(" + item[0] + "), " + str(item[1])
-            )
-        else:
-            top_10_list.append(item[0] + ", " + str(item[1]))
+    with open(file, "r") as f:
+        for i, line in enumerate(f):
+            if i % processes == rank:
+                try:
+                    line = line[:-2]
+                    line = json.loads(line)
+                except json.decoder.JSONDecodeError:
+                    print("Malformed JSON. Cannot read the line.")
+                    continue
+                except Exception as e:
+                    print(e)
 
-    return top_10_list
+                try:
+                    text = line["doc"]["text"]
+                    language = line["doc"]["metadata"]["iso_language_code"]
+                    if language != "":
+                        if language in language_occurences.keys():
+                            language_occurences[language] += 1
+                        else:
+                            language_occurences[language] = 1
+
+                    hashtags = line["doc"]["entities"]["hashtags"]
+                    for h in hashtags:
+                        hashtag = h["text"].lower()
+
+                        if hashtag in hashtag_occurences:
+                            hashtag_occurences[hashtag] += 1
+                        else:
+                            hashtag_occurences[hashtag] = 1
+                except:
+                    print("Failed to process tweet.")
+
+    return language_occurences, hashtag_occurences
 
 
 def master_tweet_processor(comm, file):
-    # Read our tweets
+    # Read tweets
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    lang_freq, hashtag_frequency = tweet_processor(rank, size, file)
-    top_10_lang = []
+    lang_frequency, hashtag_frequency = tweet_processor(rank, size, file)
 
     if size > 1:
         # multi process
-        top_10_lang, top_10_htag = marshall_freq(comm, lang_freq, hashtag_frequency)
+        languages, hashtags = marshall_freq(comm, lang_frequency, hashtag_frequency)
 
         for i in range(size - 1):
             comm.send("exit", dest=(i + 1), tag=(i + 1))
     elif size == 1:
         # 1 node 1 core 1 process
-        top_10 = sorted(lang_freq.items(), key=lambda item: item[1], reverse=True)[:10]
-        top_10_lang = match_country(top_10)
-        top_10_htag = sorted(
+        top_10 = sorted(lang_frequency.items(), key=lambda item: item[1], reverse=True)[
+            :10
+        ]
+        languages = match_country(top_10)
+        hashtags = sorted(
             hashtag_frequency.items(), key=lambda item: item[1], reverse=True
         )[:10]
 
-    print(HORIZONTAL_LINE)
-    print("-------Top 10 Languages-------")
-    print(HORIZONTAL_LINE)
-    for i in range(len(top_10_lang)):
-        print(i + 1, " : ", top_10_lang[i])
-    print(HORIZONTAL_LINE)
-    print("--------Top 10 Hashtags-----------")
-    print(HORIZONTAL_LINE)
-    for i in range(len(top_10_htag)):
-        print(i + 1, " : ", top_10_htag[i][0], top_10_htag[i][1])
+    print_top_languages(languages)
+    print_top_hashtags(hashtags)
     print(HORIZONTAL_LINE)
 
 
@@ -150,32 +180,17 @@ def slave_tweet_processor(comm, file):
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    lang_freq, hashtag_frequency = tweet_processor(rank, size, file)
-    data = [lang_freq, hashtag_frequency]
+    lang_frequency, hashtag_frequency = tweet_processor(rank, size, file)
+    data_to_send = [lang_frequency, hashtag_frequency]
     # start to listen from master
     while True:
         in_comm = comm.recv(source=MASTER_RANK, tag=rank)
         if isinstance(in_comm, str):
             if in_comm == "return_data":
                 # send back data
-                comm.send(data, dest=MASTER_RANK, tag=MASTER_RANK)
+                comm.send(data_to_send, dest=MASTER_RANK, tag=MASTER_RANK)
             elif in_comm in ("exit"):
                 exit(0)
-
-
-def timer(func):
-    """Print the runtime of the decorated function"""
-
-    @functools.wraps(func)
-    def wrapper_timer(*args, **kwargs):
-        started_at = time.perf_counter()
-        value = func(*args, **kwargs)
-        ended_at = time.perf_counter()
-        run_time = ended_at - started_at
-        print(f"Finished {func.__name__!r} in {run_time:.4f} seconds")
-        return value
-
-    return wrapper_timer
 
 
 @timer
